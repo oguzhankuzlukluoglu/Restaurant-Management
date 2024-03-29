c59ca314a986eb67c404b5406911d7fc0d067f0f
Oğuzhan <oguzhankuzlukluoglu@gmail.com>
2024-03-29 13:43:18 +0300

    .

--- models/noteModels.go
+++ models/noteModels.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Note struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Text       string             `json:"text"`
+	Title      string             `json:"title"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	Note_id    string             `json:"note_id"`
+}
--- models/orderModel.go
+++ models/orderModel.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Order struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Order_Date time.Time          `json:"Order_date" validate:"required"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	Order_id   *string            `json:"order_id"`
+	Table_id   *string            `json:"table_id" validate:"required"`
+}
