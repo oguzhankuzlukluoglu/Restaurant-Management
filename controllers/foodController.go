7d38f7fa9d733aa7ee349ac160f968bea73a6681
Oğuzhan <oguzhankuzlukluoglu@gmail.com>
2024-03-31 21:50:48 +0300

    back-end

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -10,7 +10,11 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"math"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -19,7 +23,45 @@ var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		strconv.Atoi(c.Query("recordPerPage")) //optional
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 10
+
+		}
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1
+
+		}
+		startIndex := (page - 1) * recordPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+		groupStage := bson.D{{"$group", bson.D{{"_id", bson.D{{"_id", "null"}}}, {"total_count", bson.D{{"$sum", 1}}}, {"data", bson.D{{"$push", "$$ROOT"}}}}}}
+		projectStage := bson.D{
+			{
+				"$project", bson.D{
+					{"_id", 0},
+					{"total_count", 1},
+					{"food_items", bson.D{{"$slice", bson.A{"$data", startIndex, recordPerPage}}}},
+				},
+			},
+		}
+		result, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage,
+		})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing food items"})
+		}
+		var allFoods []bson.M
+		if err = result.All(ctx, &allFoods); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allFoods[0])
 	}
 }
 func GetFood() gin.HandlerFunc {
@@ -73,14 +115,71 @@ func CreateFood() gin.HandlerFunc {
 	}
 }
 func round(num float64) int {
-	return 0
+	return int(num + math.Copysign(0.5, num))
 }
 func toFixed(num float64, precision int) float64 {
-	return 0
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+func inTimeSpan(start, end, check time.Time) bool {
+	return start.After(time.Now()) && end.After(start)
 }
 
 func UpdateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+		var food models.Food
+		foodID := c.Param("food_id")
+		if err := c.BindJSON(&menu); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var updateObj primitive.D
+		if food.Name != nil {
+			updateObj = append(updateObj, bson.E{"name", food.Name})
 
+		}
+		if food.Price != nil {
+			updateObj = append(updateObj, bson.E{"price", food.Price})
+
+		}
+		if food.Food_image != nil {
+			updateObj = append(updateObj, bson.E{"image", food.Food_image})
+
+		}
+		if food.Menu_id != nil {
+			err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+			defer cancel()
+			if err != nil {
+				msg := fmt.Sprint("message:Menu was not found")
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				return
+			}
+
+			updateObj = append(updateObj, bson.E{"menu", food.Price})
+
+		}
+		food.Updated_at, _ = time.Parse(time.RFC1123, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{"updated_at", food.Updated_at})
+		upsert := true
+		filter := bson.M{"food_id": foodID}
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+		result, err := foodCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{"set", updateObj},
+			},
+			&opt,
+		)
+		if err != nil {
+			msg := fmt.Sprint("food item update failed")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		c.JSON(http.StatusOK, result)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-playground/validator/v10 v10.19.0
 	go.mongodb.org/mongo-driver v1.14.0
 )
 
@@ -15,7 +16,6 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
