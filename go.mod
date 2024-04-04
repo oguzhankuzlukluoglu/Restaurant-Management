453f274282a24166c3a94089b65d2589e80d1739
Oğuzhan <oguzhankuzlukluoglu@gmail.com>
2024-04-04 17:36:54 +0300

    FINAL
    FINAL COMMIT

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.22
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/validator/v10 v10.19.0
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	go.mongodb.org/mongo-driver v1.14.0
+	golang.org/x/crypto v0.21.0
 )
 
 require (
@@ -34,7 +36,6 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/arch v0.7.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,9 +1,32 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"RESTAURANT-MANAGEMENT/helpers"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprint("no Authorization header provided")})
+			c.Abort()
+			return
+		}
+		claims, err := helpers.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_name)
+		c.Set("last_name", claims.Last_name)
+		c.Set("uid", claims.Uid)
+
+		c.Next()
 
 	}
 }
